41aa1204b3870231e97ea30a1a4eff86a8a178a4
applecat144 <nathi.eve.online@gmail.com>
2022-11-23 19:06:16 +0100

    Add the table display, started working on a fixed header.

--- script.js
+++ script.js
@@ -7,13 +7,42 @@ function book(name, author, pageNumber, readStatus) { //this is the book constru
     this.readStatus = readStatus;
 }
 
-function addNewBook() {
+/* function addNewBook() {
     let name = document.querySelector("#name").value
         , author = document.querySelector("#author").value
         , pageNumber = document.querySelector("#pageNumber").value
         , readStatus = document.querySelector('input[name="readStatus"]:checked').value;
-    console.log("yep");
     myLibrary[0] = new book(name, author, pageNumber, readStatus);
+    preventDefault();
+} */
+
+function displayBook() {
+
+    for (i = 0; i <= myLibrary.length; i++) {
+
+        let newBook = document.createElement('tr');
+
+        newBook.classList.add(`book${i}`);
+
+        for (let property in myLibrary[i]) {
+            let newCell = document.createElement('td');
+            newCell.textContent = `${myLibrary[i][property]}`
+            newBook.appendChild(newCell);
+            console.log("done");
+
+        }
+
+        document.querySelector('tbody').appendChild(newBook);
+        
+    }
 }
 
-document.querySelector('button').addEventListener('click', addNewBook);
+function dummyBooks() {
+    for (i = 0; i < 50; i++) {
+        myLibrary[i] = new book("azea", "trtrt", 58, "y")
+    }
+}
+
+dummyBooks();
+displayBook();
+// document.querySelector('button').addEventListener('click', addNewBook);
