b94e0abd1c68b4682c37cb54234ef07ebfb6ff33
applecat144 <nathi.eve.online@gmail.com>
2022-11-27 12:25:19 +0100

    improvement on styling : limit the width of the content. Add a toggle read button. Add a The library is empty sign whenever the library is empty.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let myLibrary = []; // this is where all the books will be stored
 let currentBook = 0;
 
+let emptyLibrary = document.createElement('td')
+
+emptyLibrary.classList.add('empty');
+emptyLibrary.textContent = "The library is empty";
+
+document.querySelector('tbody').appendChild(document.createElement('tr').appendChild(emptyLibrary));
+
 document.querySelector('.addNewBook').addEventListener('click', addNewBook);
 
 // This allow to show the book form when clicking the new book button, and hides it when
 // completing the form and adding the book or clicking the form's background
 document.querySelectorAll('.addNewBook, .form-container, .newBookBtn').forEach((item) => {
     item.addEventListener('click', () => {
-        console.log('damn')
         document.querySelector('.form-container').classList.toggle('shown');
     })
 });
@@ -42,6 +48,12 @@ function addNewBook() { //this compute new book form input to create a new book 
 
 function displayBook() {
 
+    if (!myLibrary[0]) { console.log('failure'); return; }
+
+    if (document.querySelector('.empty')) {
+        document.querySelector('tbody').removeChild(document.querySelector('.empty'));
+    }
+
     let newBook = document.createElement('tr');
 
     newBook.classList.add(`book`);
@@ -52,13 +64,21 @@ function displayBook() {
         newBook.appendChild(newCell);
     }
 
-    let removeBtnCell = document.createElement('td');
+    let btnCell = document.createElement('td');
     let removeBtn = document.createElement('button');
 
     removeBtn.classList.add('remove');
-    removeBtn.textContent = "delete";
-    removeBtnCell.appendChild(removeBtn);
-    newBook.appendChild(removeBtnCell);
+    removeBtn.textContent = "d";
+    btnCell.appendChild(removeBtn);
+
+
+    let toggleRead = document.createElement('button')
+
+    toggleRead.classList.add('toggleRead');
+    toggleRead.textContent = "?";
+    btnCell.appendChild(toggleRead)
+
+    newBook.appendChild(btnCell);
 
     document.querySelector('tbody').appendChild(newBook);
 
@@ -69,12 +89,16 @@ function displayBook() {
 
     })
 
+    document.querySelectorAll('.toggleRead').forEach((button) => {
+        button.addEventListener('click', toggleReadFunc);
+    })
+
     currentBook++;
 }
 
 function identifyBook() { //this function number the books in the order they're displayed
     let books = document.querySelectorAll(".book");
-    let i=0;
+    let i = 0;
 
     books.forEach((item) => {
         item.setAttribute('class', `book book-${i}`);
@@ -82,7 +106,7 @@ function identifyBook() { //this function number the books in the order they're 
     })
 }
 
-function removeBook (e) {
+function removeBook(e) {
     let currBook = e.target.parentNode.parentNode.classList[1];
     let currBookIndex = currBook.substr(5);
 
@@ -90,5 +114,34 @@ function removeBook (e) {
     myLibrary.splice(currBookIndex, 1);
 
     currentBook = myLibrary.length;
+
+    if (!myLibrary[0]) {
+        emptyLibrary.classList.add('empty');
+        emptyLibrary.textContent = "The library is empty";
+
+        document.querySelector('tbody').appendChild(document.createElement('tr').appendChild(emptyLibrary));
+    }
 }
 
+function addALot() {
+
+    for (i = 0; i < 50; i++) {
+        document.querySelector('.addNewBook').click();
+        document.querySelector('.newBookBtn').click();
+    }
+}
+
+function toggleReadFunc(e) {
+
+    let currBook = e.target.parentNode.parentNode.classList[1];
+    let currBookIndex = currBook.substr(5);
+
+    if (myLibrary[currBookIndex]['readStatus'] == '1') {
+        myLibrary[currBookIndex]['readStatus'] = '0';
+        document.querySelector(`.${currBook} :nth-child(4)`).textContent = "0";
+    } else {
+        myLibrary[currBookIndex]['readStatus'] = '1';
+        document.querySelector(`.${currBook} :nth-child(4)`).textContent = "1";
+    }
+
+}
