08e59dc2f8ab3bda7ab3276abf3acdc77fde2919
applecat144 <nathi.eve.online@gmail.com>
2022-11-27 09:34:58 +0100

    added delete button to remove a book from the display and the internal library.

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function displayBook() {
 
     let newBook = document.createElement('tr');
 
-    newBook.classList.add(`book${currentBook}`);
+    newBook.classList.add(`book`);
 
     for (let property in myLibrary[currentBook]) {
         let newCell = document.createElement('td');
@@ -52,7 +52,43 @@ function displayBook() {
         newBook.appendChild(newCell);
     }
 
+    let removeBtnCell = document.createElement('td');
+    let removeBtn = document.createElement('button');
+
+    removeBtn.classList.add('remove');
+    removeBtn.textContent = "delete";
+    removeBtnCell.appendChild(removeBtn);
+    newBook.appendChild(removeBtnCell);
+
     document.querySelector('tbody').appendChild(newBook);
+
+    identifyBook();
+
+    document.querySelectorAll('.remove').forEach((button) => {
+        button.addEventListener('click', removeBook);
+
+    })
 
     currentBook++;
 }
+
+function identifyBook() { //this function number the books in the order they're displayed
+    let books = document.querySelectorAll(".book");
+    let i=0;
+
+    books.forEach((item) => {
+        item.setAttribute('class', `book book-${i}`);
+        i++;
+    })
+}
+
+function removeBook (e) {
+    let currBook = e.target.parentNode.parentNode.classList[1];
+    let currBookIndex = currBook.substr(5);
+
+    document.querySelector('tbody').removeChild(document.querySelector(`.${currBook}`));
+    myLibrary.splice(currBookIndex, 1);
+
+    currentBook = myLibrary.length;
+}
+
